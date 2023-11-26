c281a2eb456e7830c30243afd2891ffdcddf6c72
Tyuiu.PopkovSV <popkovsergej10@gmail.com>
2023-11-26 19:43:05 +0300

    Task7

---
+++ Tyuiu.PopkovSV.Sprint5.Task7.V4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PopkovSV.Sprint5.Task7.V4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PopkovSV.Sprint5.Task7.V4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4588f7db-6c73-484b-a989-7b65210a1663")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PopkovSV.Sprint5.Task7.V4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PopkovSV.Sprint5.Task7.V4.Lib;
+using System.IO;
+
+namespace Tyuiu.PopkovSV.Sprint5.Task7.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService DataService = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Попков С. В. | ИСТНб-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Обработка текстовых файлов                                              *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Попков Сергей Вадимович | ИСТНб-23-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:|DataSprint5|InPutDataFileTask7V4.txt в котором есть набор   *");
+            Console.WriteLine("* символьных данных.Заменить все русские буквы на #.Полученный результат  *");
+            Console.WriteLine("* сохранить в файл                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = $@"C:\DataSprint5\InPutDataFileTask7V4.txt";
+            string res = File.ReadAllText(path);
+            Console.WriteLine(res); Console.WriteLine("Файл находится тут: " + path);
+
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                     ");
+            Console.WriteLine("****************************************************************");
+
+            string w = DataService.LoadDataAndSave(path);
+            Console.WriteLine("Cоздан!");
+            Console.ReadKey();
+        }
+    }
+}
